0f10eb5f4f825f240b2d0ba225315fd42ae1e191
acys159 <changacysaaron@gmail.com>
2025-08-01 13:09:24 +0800

    Add project files.

---
+++ PetGrooming/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PetGrooming.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
