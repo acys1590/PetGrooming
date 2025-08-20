5bc5d98b834f76ca1c4cbaa0b263cb8fd669a607
Devon-888 <devoneyc-wm24@student.tarc.edu.my>
2025-08-20 13:57:18 +0800

    kosdk

--- PetGrooming/Controllers/LoginVM.cs
+++ PetGrooming/Controllers/LoginVM.cs
@@ -3,7 +3,7 @@
     public class LoginVM
     {
         public bool RememberMe { get; internal set; }
-        public string Password { get; internal set; }
+        public string? Password { get; internal set; }
         public object?[]? Email { get; internal set; }
     }
 }
